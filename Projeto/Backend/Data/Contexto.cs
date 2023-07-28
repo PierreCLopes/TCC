5437b72cdd33d3941e01bf1f9160758b91af9155
PierreCLopes <pierre.clopes@gmail.com>
2023-07-28 19:09:00 -0300

    Criando projeto

---
+++ Projeto/Backend/Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ Projeto/Backend/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    [Table("USUARIO")]
+    public class Usuario
+    {
+        [Key]
+        [Column("HANDLE")]
+        public int Handle { get; set; }
+    }
+}
