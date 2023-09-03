a27b6efda1f9d4b3d3c5aa11f39df24e49bc6c28
PierreCLopes <pierre.clopes@gmail.com>
2023-09-02 21:13:05 -0300

    Adicionado responsividade menu lateral

--- Projeto/frontend/src/shared/components/menu-lateral/MenuLateral.tsx
+++ Projeto/frontend/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material"
 import { ReactNode } from "react";
+import { useDrawerContext } from "../../contexts";
 
 interface IMenuLateralProps {
     children: ReactNode;
@@ -7,11 +8,14 @@ interface IMenuLateralProps {
 
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 
     return(
         <>
-        <Drawer variant='permanent'>
-            <Box width={theme.spacing(28)} height={'100%'} display={'flex'} flexDirection={'column'}>
+        <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+            <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
                 <Box width='100%' height={theme.spacing(20)} display={'flex'} alignItems={'center'} justifyContent={'center'}>
                     <Avatar sx={{height: theme.spacing(12), width: theme.spacing(12)}} src="https://s2.glbimg.com/cNcGRzTnqH-uXWP2RF66__GneE8=/0x0:439x273/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/U/C/V9PkuMSEu3hQDv1bzUmg/screenshot-201.png"/>
                 </Box>
@@ -31,7 +35,7 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
             </Box>
         </Drawer>
 
-        <Box height='100vh' marginLeft={theme.spacing(28)}>
+        <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
             { children }
         </Box>
         </>
---
+++ Projeto/frontend/src/shared/contexts/DrawerContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useCallback, useMemo, useState, ReactNode, useContext } from "react";
+
+interface IDrawerContextData {
+  isDrawerOpen: boolean;
+  toggleDrawerOpen: () => void;
+}
+
+interface IAppDrawerProviderProps {
+  children: ReactNode;
+}
+
+const DrawerContext = createContext({} as IDrawerContextData);
+
+export const useDrawerContext = () => {
+    return useContext(DrawerContext);
+}
+
+export const DrawerProvider: React.FC<IAppDrawerProviderProps> = ({ children }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawerOpen = useCallback(() => {
+    setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen );
+  }, []);
+
+  return (
+    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+      {children}
+    </DrawerContext.Provider>
+  );
+}
